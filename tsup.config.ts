8a7676579c60e5b65a6a9e98fcf2e91534e60a9c
Arya Emami <aryaemami59@yahoo.com>
2024-03-07 10:31:28 -0600

    Update `tsup.config.ts`

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,38 +1,24 @@
-import type { Options } from "tsup"
-import { defineConfig } from "tsup"
+import { resolve } from 'node:path'
+import type { Options } from 'tsup'
+import { defineConfig } from 'tsup'
 
 export default defineConfig(options => {
   const commonOptions: Options = {
     sourcemap: true,
     clean: true,
-    dts: true,
-
+    tsconfig: resolve('tsconfig.build.json'),
     ...options,
   }
 
   return [
     {
       ...commonOptions,
-      entry: ["src/index.ts"],
-      format: ["esm", "cjs"],
-      // outExtension: ({ format }) => ({
-      //   js: format === "esm" ? `.mjs` : ".cjs",
-      //   dts: format === "cjs" ? ".d.cts" : ".d.mts",
-      // }),
-    },
-
-    // {
-    //   ...commonOptions,
-    //   format: ["cjs"],
-    //   outExtension: () => ({ js: ".cjs" }),
-    // },
-    {
-      entry: ["src/bin/cli.ts"],
-      sourcemap: true,
-      clean: true,
       dts: true,
-      format: ["esm", "cjs"],
-      outExtension: ({ format }) => ({ js: format === "esm" ? ".mjs" : ".js" }),
+      entry: ['src/index.ts'],
+    },
+    {
+      ...commonOptions,
+      entry: ['src/bin/cli.ts'],
     },
   ]
 })
